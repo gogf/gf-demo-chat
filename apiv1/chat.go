d1950cda78e38f2e6777aeb19da54d164a43bed2
john <john@johng.cn>
2022-03-03 10:01:59 +0800

    up

--- apiv1/chat.go
+++ apiv1/chat.go
@@ -5,14 +5,14 @@ import (
 )
 
 type ChatIndexReq struct {
-	g.Meta `path:"/chat" method:"get"  tags:"Chat" summary:"Chat homepage"`
+	g.Meta `path:"/chat" method:"get"  tags:"ChatService" summary:"Home page"`
 }
 type ChatIndexRes struct {
 	g.Meta `mime:"text/html" type:"string" example:"<html/>"`
 }
 
 type ChatNameReq struct {
-	g.Meta `path:"/chat/name" method:"post"  tags:"Chat" summary:"Customize chat name page"`
+	g.Meta `path:"/chat/name" method:"post"  tags:"ChatService" summary:"Name page"`
 	Name   string `v:"required|max-length:21#Why not an awesome name"`
 }
 type ChatNameRes struct {
@@ -20,7 +20,7 @@ type ChatNameRes struct {
 }
 
 type ChatWebsocketReq struct {
-	g.Meta `path:"/chat/websocket" method:"get"  tags:"Chat" summary:"Send message"`
+	g.Meta `path:"/chat/websocket" method:"get"  tags:"ChatService" summary:"Send message"`
 }
 type ChatWebsocketRes struct {
 	g.Meta `mime:"text/html" type:"string" example:"<html/>" dc:"It redirects to homepage if success"`
